e7b3b770e051dc44d455aaca1f7f39d044aadd1d
Ahmad <hamada_1418@hotmail.com>
2020-06-09 13:14:50 +0300

    my first commit

---
+++ website/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+# Create your views here.
+def home(request):
+
+    if request.method == "POST":
+        fname = request.POST.get('fname', '');
+        lname = request.POST.get('lname', '');
+        email = request.POST.get('email', '');
+        subject= request.POST.get('subject', '');
+        message = request.POST.get('message', '');
+
+        send_mail(
+          'Website message - ' + fname + " " + lname + ' - Phone Number:' + subject,
+           message,
+           email,
+           ['hamada_1418@hotmail.com'],
+           fail_silently=False,
+
+           )
+
+        return render(request, 'home.html', {})
+    else:
+        return render(request, 'home.html', {})
