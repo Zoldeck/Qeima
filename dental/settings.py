d015f490d78af6afab8656df637579da5e098cfe
Ahmad <hamada_1418@hotmail.com>
2020-06-11 02:23:57 +0300

    added heroku

--- dental/settings.py
+++ dental/settings.py
@@ -128,19 +128,14 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import os
-'''
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hamadaksa@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('Emailksa')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-'''
 
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
+
 
 django_heroku.settings(locals())
